ddccb557d6a6e0435bc11552cf91a34a1cb3921a
AwixStudio <awixstudio@gmail.com>
2026-07-20 03:55:19 +0200

    Adding exsiting repositories.

---
+++ GitDesktop/UI/MainView.cs
@@ -0,0 +1,22 @@
+﻿using GitDesktop.Repository;
+using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitDesktop.UI
+{
+    internal class MainView(RepositoryManager repositoryManager) : IRender
+    {
+        private readonly RepositoryManager repositoryManager = repositoryManager;
+
+        public void Render()
+        {
+            ImGui.BeginMainMenuBar();
+
+
+
+            ImGui.EndMainMenuBar();
+        }
+    }
+}
